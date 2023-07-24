05e24da0430976bc2539720174510d5836c23d14
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2023-07-24 11:03:23 +0530

    Completed this by Own

---
+++ perfect-guess-game.py
@@ -0,0 +1,18 @@
+import random
+
+randomNumber = random.randint(1, 101)
+userGuess = None
+guesses = 0
+
+while userGuess != randomNumber:
+    userGuess = int(input("Please enter a Number : "))
+    guesses += 1
+    if userGuess == randomNumber:
+        print("You won the game!!!")
+    else:
+        if userGuess > randomNumber:
+            print("You Guessed too High!!") 
+        elif userGuess < randomNumber:
+            print("You Guessed too Low!!!")
+        
+print(f"You Guessed in {guesses} turns.")
